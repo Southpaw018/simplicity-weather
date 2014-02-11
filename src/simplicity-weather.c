9dbace96ceeac16a6f62d76bfa0881f30dc1d9a2
Jim Drey <moof@moofit.com>
2014-02-11 14:04:55 -0800

    General cleanup. Convert to tab indentation.

--- src/simplicity-weather.c
+++ src/simplicity-weather.c
@@ -14,182 +14,182 @@ static AppSync sync;
 static uint8_t sync_buffer[64];
 
 enum WeatherKey {
-  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
-  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
-  WEATHER_CITY_KEY = 0x2,         // TUPLE_CSTRING
+	WEATHER_ICON_KEY = 0x0,			// TUPLE_INT
+	WEATHER_TEMPERATURE_KEY = 0x1,	// TUPLE_CSTRING
+	WEATHER_CITY_KEY = 0x2,			// TUPLE_CSTRING
 };
 
 static const uint32_t WEATHER_ICONS[] = {
-  RESOURCE_ID_IMAGE_SUN, //0
-  RESOURCE_ID_IMAGE_CLOUD, //1
-  RESOURCE_ID_IMAGE_RAIN, //2
-  RESOURCE_ID_IMAGE_SNOW //3
+	RESOURCE_ID_IMAGE_SUN,		//0
+	RESOURCE_ID_IMAGE_CLOUD,	//1
+	RESOURCE_ID_IMAGE_RAIN,		//2
+	RESOURCE_ID_IMAGE_SNOW		//3
 };
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
 }
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
-  switch (key) {
-    case WEATHER_ICON_KEY:
-      if (icon_bitmap) {
-        gbitmap_destroy(icon_bitmap);
-      }
-      icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
-      bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
-      break;
+	switch (key) {
+		case WEATHER_ICON_KEY:
+			if (icon_bitmap) {
+				gbitmap_destroy(icon_bitmap);
+			}
+			icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
+			bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+			break;
 
-    case WEATHER_TEMPERATURE_KEY:
-      // App Sync keeps new_tuple in sync_buffer, so we may use it directly
-      text_layer_set_text(temperature_layer, new_tuple->value->cstring);
-      break;
+		case WEATHER_TEMPERATURE_KEY:
+			// App Sync keeps new_tuple in sync_buffer, so we may use it directly
+			text_layer_set_text(temperature_layer, new_tuple->value->cstring);
+			break;
 
-    case WEATHER_CITY_KEY:
-      text_layer_set_text(city_layer, new_tuple->value->cstring);
-      break;
-  }
+		case WEATHER_CITY_KEY:
+			text_layer_set_text(city_layer, new_tuple->value->cstring);
+			break;
+	}
 }
 
 static void send_cmd(void) {
-  Tuplet value = TupletInteger(1, 1);
+	Tuplet value = TupletInteger(1, 1);
 
-  DictionaryIterator *iter;
-  app_message_outbox_begin(&iter);
+	DictionaryIterator *iter;
+	app_message_outbox_begin(&iter);
 
-  if (iter == NULL) {
-    return;
-  }
+	if (iter == NULL) {
+		return;
+	}
 
-  dict_write_tuplet(iter, &value);
-  dict_write_end(iter);
+	dict_write_tuplet(iter, &value);
+	dict_write_end(iter);
 
-  app_message_outbox_send();
+	app_message_outbox_send();
 }
 
 void line_layer_update_callback(Layer *layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
 void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
-  static char date_text[] = "Xxxxxxxxx 00";
+	// Need to be static because they're used by the system later.
+	static char time_text[] = "00:00";
+	static char date_text[] = "Xxxxxxxxx 00";
 
-  char *time_format;
-
-
-  // TODO: Only update the date when it's changed.
-  strftime(date_text, sizeof(date_text), "%B %e", tick_time);
-  text_layer_set_text(text_date_layer, date_text);
+	char *time_format;
 
 
-  if (clock_is_24h_style()) {
-    time_format = "%R";
-  } else {
-    time_format = "%I:%M";
-  }
+	// TODO: Only update the date when it's changed.
+	strftime(date_text, sizeof(date_text), "%B %e", tick_time);
+	text_layer_set_text(text_date_layer, date_text);
 
-  strftime(time_text, sizeof(time_text), time_format, tick_time);
 
-  // Kludge to handle lack of non-padded hour format string
-  // for twelve hour clock.
-  if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-  }
+	if (clock_is_24h_style()) {
+		time_format = "%R";
+	} else {
+		time_format = "%I:%M";
+	}
 
-  text_layer_set_text(text_time_layer, time_text);
+	strftime(time_text, sizeof(time_text), time_format, tick_time);
+
+	// Kludge to handle lack of non-padded hour format string
+	// for twelve hour clock.
+	if (!clock_is_24h_style() && (time_text[0] == '0')) {
+		memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+	}
+
+	text_layer_set_text(text_time_layer, time_text);
 }
 
 void deinit(void) {
-  tick_timer_service_unsubscribe();
-  window_destroy(window);
+	tick_timer_service_unsubscribe();
+	window_destroy(window);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
+	Layer *window_layer = window_get_root_layer(window);
 
-  text_date_layer = text_layer_create(GRect(8, 68, 144-8, 168-68));
-  text_layer_set_text_color(text_date_layer, GColorWhite);
-  text_layer_set_background_color(text_date_layer, GColorClear);
-  text_layer_set_font(text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
-  layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
+	text_date_layer = text_layer_create(GRect(8, 68, 144-8, 168-68));
+	text_layer_set_text_color(text_date_layer, GColorWhite);
+	text_layer_set_background_color(text_date_layer, GColorClear);
+	text_layer_set_font(text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+	layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
 
-  text_time_layer = text_layer_create(GRect(7, 92, 144-7, 168-92));
-  text_layer_set_text_color(text_time_layer, GColorWhite);
-  text_layer_set_background_color(text_time_layer, GColorClear);
-  text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
-  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+	text_time_layer = text_layer_create(GRect(7, 92, 144-7, 168-92));
+	text_layer_set_text_color(text_time_layer, GColorWhite);
+	text_layer_set_background_color(text_time_layer, GColorClear);
+	text_layer_set_font(text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
+	layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
-  GRect line_frame = GRect(8, 97, 139, 2);
-  line_layer = layer_create(line_frame);
-  layer_set_update_proc(line_layer, line_layer_update_callback);
-  layer_add_child(window_layer, line_layer);
+	GRect line_frame = GRect(8, 97, 139, 2);
+	line_layer = layer_create(line_frame);
+	layer_set_update_proc(line_layer, line_layer_update_callback);
+	layer_add_child(window_layer, line_layer);
 
-  icon_layer = bitmap_layer_create(GRect(32, 10, 80, 80));
-  layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
+	icon_layer = bitmap_layer_create(GRect(32, 10, 80, 80));
+	layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
 
-  temperature_layer = text_layer_create(GRect(0, 95, 144, 68));
-  text_layer_set_text_color(temperature_layer, GColorWhite);
-  text_layer_set_background_color(temperature_layer, GColorClear);
-  text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
+	temperature_layer = text_layer_create(GRect(0, 95, 144, 68));
+	text_layer_set_text_color(temperature_layer, GColorWhite);
+	text_layer_set_background_color(temperature_layer, GColorClear);
+	text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
+	layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
 
-  city_layer = text_layer_create(GRect(0, 125, 144, 68));
-  text_layer_set_text_color(city_layer, GColorWhite);
-  text_layer_set_background_color(city_layer, GColorClear);
-  text_layer_set_font(city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_alignment(city_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(city_layer));
+	city_layer = text_layer_create(GRect(0, 125, 144, 68));
+	text_layer_set_text_color(city_layer, GColorWhite);
+	text_layer_set_background_color(city_layer, GColorClear);
+	text_layer_set_font(city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(city_layer, GTextAlignmentCenter);
+	layer_add_child(window_layer, text_layer_get_layer(city_layer));
 
-  Tuplet initial_values[] = {
-    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
-    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
-    TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
-  };
+	Tuplet initial_values[] = {
+		TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+		TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
+		TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
+	};
 
-  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
-      sync_tuple_changed_callback, sync_error_callback, NULL);
+	app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+			sync_tuple_changed_callback, sync_error_callback, NULL);
 
-  send_cmd();
+	send_cmd();
 }
 
 static void window_unload(Window *window) {
-  app_sync_deinit(&sync);
+	app_sync_deinit(&sync);
 
-  if (icon_bitmap) {
-    gbitmap_destroy(icon_bitmap);
-  }
+	if (icon_bitmap) {
+		gbitmap_destroy(icon_bitmap);
+	}
 
-  text_layer_destroy(city_layer);
-  text_layer_destroy(temperature_layer);
-  bitmap_layer_destroy(icon_layer);
+	text_layer_destroy(city_layer);
+	text_layer_destroy(temperature_layer);
+	bitmap_layer_destroy(icon_layer);
 }
 
 void init(void) {
-  window = window_create();
-  window_set_background_color(window, GColorBlack);
+	window = window_create();
+	window_set_background_color(window, GColorBlack);
 
-  //window_set_fullscreen(window, true); //don't need for watchface?
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload
-  });
+	//window_set_fullscreen(window, true); //don't need for watchface?
+	window_set_window_handlers(window, (WindowHandlers) {
+		.load = window_load,
+		.unload = window_unload
+	});
 
-  const int inbound_size = 64;
-  const int outbound_size = 64;
-  app_message_open(inbound_size, outbound_size);
+	const int inbound_size = 64;
+	const int outbound_size = 64;
+	app_message_open(inbound_size, outbound_size);
 
-  const bool animated = true;
-  window_stack_push(window, animated);
+	const bool animated = true;
+	window_stack_push(window, animated);
 
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+	init();
+	app_event_loop();
+	deinit();
 }
