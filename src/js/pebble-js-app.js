d21085a67093260ecfc1af8db3be3d91e074f69f
Jim Drey <moof@moofit.com>
2014-02-16 13:17:00 -0800

    Replace JS w Niknam's Futura Weather ver

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,77 +1,78 @@
-function iconFromWeatherId(weatherId) {
-	if (weatherId < 600) {
-		return 2;
-	} else if (weatherId < 700) {
-		return 3;
-	} else if (weatherId > 800) {
-		return 1;
-	} else {
-		return 0;
+//Partially adapted (and mostly stolen) from github.com/Niknam/futura-weather-sdk2.0 / src / js / pebble-js-app.js
+
+Pebble.addEventListener("ready", function(e) {
+	console.log("Starting ...");
+	updateWeather();
+});
+
+Pebble.addEventListener("appmessage", function(e) {
+	console.log("Got a message - Starting weather request...");
+	updateWeather();
+});
+
+var updateInProgress = false;
+
+function updateWeather() {
+	if (!updateInProgress) {
+		updateInProgress = true;
+		var locationOptions = { "timeout": 15000, "maximumAge": 60000 };
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
 	}
+	else {
+		console.log("Not starting a new request. Another one is in progress...");
+	}
+}
+
+function locationSuccess(pos) {
+	var coordinates = pos.coords;
+	console.log("Got coordinates: " + JSON.stringify(coordinates));
+	fetchWeather(coordinates.latitude, coordinates.longitude);
+}
+
+function locationError(err) {
+	console.warn('Location error (' + err.code + '): ' + err.message);
+	Pebble.sendAppMessage({ "error": "Loc unavailable" });
+	updateInProgress = false;
 }
 
 function fetchWeather(latitude, longitude) {
 	var response;
 	var req = new XMLHttpRequest();
-	req.open('GET', "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude, true);
+	req.open('GET', "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude + "&cnt=1", true);
 	req.onload = function(e) {
 		if (req.readyState == 4) {
 			if(req.status == 200) {
 				console.log(req.responseText);
 				response = JSON.parse(req.responseText);
-				var temperature, icon, city;
-				if (response && Object.keys(response).length > 0) {
-					var weatherResult = response;
-					temperature = Math.round((weatherResult.main.temp - 273.15) * 1.8 + 32);
-					icon = iconFromWeatherId(weatherResult.weather[0].id);
-					city = weatherResult.name;
-					console.log(temperature);
-					console.log(icon);
-					console.log(city);
+				var temperature, icon, city, sunrise, sunset, condition;
+				var current_time = Date.now() / 1000;
+				if (response) {
+					var tempResult = response.main.temp;
+					if (response.sys.country === "US") {
+						// Convert temperature to Fahrenheit if user is within the US
+						temperature = Math.round(((tempResult - 273.15) * 1.8) + 32);
+					}
+					else {
+						// Otherwise, convert temperature to Celsius
+						temperature = Math.round(tempResult - 273.15);
+					}		 
+					//var condition = response.weather[0].id;
+					var city = response.name;
+
+					console.log("Temperature: " + temperature + " City: " + city);
+							  
 					Pebble.sendAppMessage({
-						"icon":icon,
-						"temperature":temperature + "\u00B0F",
-						"city":city});
+						"city": city,
+						"temperature": temperature,
+					});
+					updateInProgress = false;
 				}
 			} else {
 				console.log("Error");
+				updateInProgress = false;
+				Pebble.sendAppMessage({ "error": "HTTP Error" });
 			}
 		}
-	}
+	};
 	req.send(null);
 }
-
-function locationSuccess(pos) {
-	var coordinates = pos.coords;
-	fetchWeather(coordinates.latitude, coordinates.longitude);
-}
-
-function locationError(err) {
-	console.warn('location error (' + err.code + '): ' + err.message);
-	Pebble.sendAppMessage({
-		"city":"Loc Unavailable",
-		"temperature":"N/A"
-	});
-}
-
-var locationOptions = { "timeout": 15000, "maximumAge": 60000 }; 
-
-
-Pebble.addEventListener("ready", function(e) {
-	console.log("connect!" + e.ready);
-	locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
-	console.log(e.type);
-});
-
-Pebble.addEventListener("appmessage", function(e) {
-	window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
-	console.log(e.type);
-	console.log(e.payload.temperature);
-	console.log("message!");
-});
-
-Pebble.addEventListener("webviewclosed", function(e) {
-	console.log("webview closed");
-	console.log(e.type);
-	console.log(e.response);
- });
